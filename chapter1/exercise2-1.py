cb09a39109776b0ae32633c18364ae17d992ca34
Emmanuel Odoom <emmanuel@example.com>
2026-07-21 09:09:31 +0000

    final update to the exercise 2-1

---
+++ chapter1/exercise2-1.py
@@ -0,0 +1,13 @@
+#42=n is a syntax error
+
+#x=y=1 works. doing a print(x,y) gives a response of '1 1' 
+
+
+#a semicolumn after a statement still runs
+
+
+
+#x=5., print(x) gives a 5.0 as a result. it works for integers only. another way to float a figure
+
+
+#xy  is a name error
