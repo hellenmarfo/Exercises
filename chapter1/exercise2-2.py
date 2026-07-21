e8a4bb4195a7e3d1ae82d8f79f51e99a20ca1902
Emmanuel Odoom <emmanuel@example.com>
2026-07-21 09:31:17 +0000

    done

--- chapter1/exercise2-2.py
+++ chapter1/exercise2-2.py
@@ -1,1 +1,13 @@
- 
+#answer 1
+ #523.5987755982989
+
+
+ #answer 2
+ #$0.75 each
+
+
+ #answer 3
+ #$945.45
+
+
+ #For some i solved using paper. the last answer i use chatGPT. i want to learn code not crack my brain on maths
